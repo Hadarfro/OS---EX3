f4b20f6ddfeab7ffe4e38b779ffc64e73a6c15a2
May Rozen <154542312+MayRozen@users.noreply.github.com>
2024-06-26 18:02:26 +0300

    :(

--- Q3/algo3.cpp
+++ Q3/algo3.cpp
@@ -151,8 +151,8 @@ int main(int argc, char* argv[]){
         cin >> commend;
         if(commend == "Newedge"){
             cout << "enter i and j: ";
-                cin >> num1;
-                cin >> num2;
+            cin >> num1;
+            cin >> num2;
             obj.Newedge(num1,num2,edges);
         }
 
@@ -160,8 +160,8 @@ int main(int argc, char* argv[]){
         cin >> commend;
         if(commend == "Removeedge"){
             cout << "enter i and j: ";
-                cin >> num1;
-                cin >> num2;
+            cin >> num1;
+            cin >> num2;
             obj.Removeedge(num1,num2,edges);
         }
     }
